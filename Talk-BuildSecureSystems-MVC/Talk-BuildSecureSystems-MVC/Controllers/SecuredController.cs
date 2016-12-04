fb67830f5e60fa14fb55fc2b08e5112663faed1b
Seth Petry-Johnson <seth@petry-johnson.com>
2016-12-04 12:39:09 -0500

    Stubbed in partial impl of [RequiredPermission]

---
+++ Talk-BuildSecureSystems-MVC/Talk-BuildSecureSystems-MVC/Controllers/SecuredController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Talk_BuildSecureSystems_MVC.Framework.Authentication;
+using Talk_BuildSecureSystems_MVC.Framework.Extensions;
+using Talk_BuildSecureSystems_MVC.Models;
+
+namespace Talk_BuildSecureSystems_MVC.Controllers {
+	public class SecuredController : Controller {
+
+		public ApplicationUser CurrentUser {
+			get {
+				if (this.m_currentUser == null) {
+					this.m_currentUser = new ApplicationUser();	// TODO
+				}
+				return this.m_currentUser;
+			}
+			set { this.m_currentUser = value; }
+		}
+		private ApplicationUser m_currentUser;
+
+
+		protected override void OnActionExecuting(ActionExecutingContext context) {
+
+			var permAttribute = context.ActionDescriptor.GetAttribute<RequiredPermissionAttribute>();
+
+			if (permAttribute == null) {
+				return;
+			}
+
+			var requiredPerm = permAttribute.Permission;
+			var hasRequiredPerm = CurrentUser.Permissions.Any(p => p.Id == (int)requiredPerm);
+
+			if (!hasRequiredPerm) {
+				context.Result = new HttpUnauthorizedResult();
+			}
+		}
+	}
+}
---
+++ Talk-BuildSecureSystems-MVC/Talk-BuildSecureSystems-MVC/Framework/Authentication/RequiredPermissionAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Talk_BuildSecureSystems_MVC.Models;
+
+namespace Talk_BuildSecureSystems_MVC.Framework.Authentication {
+
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
+	public class RequiredPermissionAttribute : Attribute {
+
+		public PermissionEnum Permission { get; private set; }
+
+		public RequiredPermissionAttribute(PermissionEnum permission) {
+			Permission = permission;
+		}
+	}
+}
