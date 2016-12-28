127080077accf51a98916e3ea62c441b2cad6caa
Seth Petry-Johnson <seth@petry-johnson.com>
2016-12-28 00:01:34 -0500

    Rough draft of some security audit stuff w/ Roslyn

---
+++ SecureFrameworkDemo/Web/Controllers/DemoApp/SecurityAuditController.cs
@@ -0,0 +1,78 @@
+﻿using System.Linq;
+using System.IO;
+using System.Web.Mvc;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace SecureFrameworkDemo.Controllers {
+
+	/// <summary>
+	/// This demonstrates some static analysis auditing techniques, exposed via a web endpoint
+	/// simply for ease of demonstration.
+	/// 
+	/// In a real app, you probably shouldn't expose this via the UI. Turn it into a console app
+	/// or something and invoke it from your build tool.
+	/// 
+	/// (For bonus points, use http://approvaltests.com/ to track the endpoint security report artifact 
+	/// over time and require manual signoff any time it changes)
+	/// </summary>
+	public class SecurityAuditController : SecureControllerBase {
+
+		public ActionResult Index() {
+			var controllerPath = @"C:\GitHub\Talk---Dont-Write-Secure-Code-Design-Secure-Systems\SecureFrameworkDemo\Web\Controllers\DemoApp\SecureFrameworkController.cs";
+			using (var stream = System.IO.File.OpenRead(controllerPath)) {
+				var tree = CSharpSyntaxTree.ParseText(SourceText.From(stream), path: controllerPath);
+				var root = tree.GetRoot();
+
+				ClassDeclarationSyntax controller = root.DescendantNodes()
+					.OfType<ClassDeclarationSyntax>()
+					.First();
+
+				var methods =
+					from m in root.DescendantNodes().OfType<MethodDeclarationSyntax>()
+					where m.Modifiers.ToString().Contains("public")
+					select m;
+
+				string output = "";
+
+				foreach (var m in methods) {
+					var attr = m.AttributeLists.SelectMany(x => x.Attributes).Where(a => a.Name.ToString().Contains("RequiredPermission"))?.FirstOrDefault();
+
+					if (attr == null)
+						continue;
+
+					var perm = attr.ArgumentList.Arguments.First();
+
+					output += m.Identifier + ": " + perm.ToString();
+				}
+
+				ViewBag.Output = output;
+
+				return View();
+			}
+		}
+
+		public bool IsController(SyntaxNode root, string args) {
+			bool isValid = false;
+
+			try {
+				isValid = 
+					root.DescendantNodes()
+						.OfType<BaseTypeSyntax>().First().ToString()
+						.Equals("ApiController") 
+					||
+					root.DescendantNodes()
+						.OfType<BaseTypeSyntax>().First().ToString()
+						.Equals("Controller");
+
+			}
+			catch {
+
+			}
+
+			return isValid;
+		}
+	}
+}
