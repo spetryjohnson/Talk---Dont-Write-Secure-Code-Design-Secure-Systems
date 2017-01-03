59c111090bfda258a3c23671cc12b4543b88ba37
Seth Petry-Johnson <seth@petry-johnson.com>
2017-01-02 21:55:37 -0500

    Added post-build event to run security audit

--- SecureFrameworkDemo/SecurityAudit/Program.cs
+++ SecureFrameworkDemo/SecurityAudit/Program.cs
@@ -29,15 +29,24 @@ namespace SecurityAudit {
 		}
 
 		private static string GetOutputPathFromArgs(string[] args) {
+			var defaultDir = Environment.CurrentDirectory;
+			var defaultFilename = $"output-{DateTime.Now.ToString("yyyyMMddHHmmss")}.csv";
+
 			// would have prefered to use CommandLineParser nuget package, but decided not to
 			// take a dependency in the demo
 			if (args.Length == 0) {
-				var currentDir = Environment.CurrentDirectory;
-				var defaultFilename = $"output-{DateTime.Now.ToString("yyyyMMddHHmmss")}.csv";
-				return Path.Combine(currentDir, defaultFilename);
+				return Path.Combine(defaultDir, defaultFilename);
 			}
+			else {
+				// if a value was provided, but it was only a directory name, create a filename by default
+				var outputFile = args[0];
 
-			return args[0];
+				if (Directory.Exists(outputFile)) {
+					outputFile = Path.Combine(outputFile, defaultFilename);
+				}
+
+				return outputFile;
+			}
 		}
 	}
 }
